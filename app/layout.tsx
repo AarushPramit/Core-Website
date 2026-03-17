e861a9e039b43e2527068ce0b2839acd4bf558b7
AarushPramit <aarushpramit062005@gmail.com>
2026-03-17 13:38:26 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Core Website",
+  description: "Clothing brand website",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
