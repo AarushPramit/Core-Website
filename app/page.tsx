29c5ca639fc0f5d43eb8907b69ab0a147c53cfd3
AarushPramit <aarushpramit062005@gmail.com>
2026-03-17 13:36:59 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,35 @@
+import Footer from "@/components/footer"
+import AnimatedHero from "@/components/ui/animated-hero"
+import Navbar from "@/components/navbar"
+import HeroScroll from "@/components/hero-scroll"
+import ProductGrid from "@/components/product-grid"
+import HorizontalScroll from "@/components/horizontal-scroll"
+import TestimonialsSection from "@/components/testimonials"
+import Campaign from "@/components/campaign"
+import Lookbook from "@/components/lookbook"
 
+export default function Home() {
+  return (
+    <main className="bg-white text-black">
+
+      <Navbar />
+
+      <HeroScroll />
+
+      <ProductGrid />
+
+      <Campaign />
+
+      <AnimatedHero />
+
+      <Lookbook />
+
+      <HorizontalScroll />
+
+      <TestimonialsSection />
+
+      <Footer />
+
+    </main>
+  );
+}
