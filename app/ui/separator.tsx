90c5d10ef613035ea6c028f8eb52ce77d387d3fc
AarushPramit <aarushpramit062005@gmail.com>
2026-03-27 22:40:30 +0530

    add missing files

---
+++ app/ui/separator.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import * as React from "react"
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
+
+// simple class merge helper
+function cn(...classes: (string | undefined)[]) {
+  return classes.filter(Boolean).join(" ")
+}
+
+const Separator = React.forwardRef<
+  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(
+({ className, orientation = "horizontal", decorative = true, ...props }, ref) => (
+  <SeparatorPrimitive.Root
+    ref={ref}
+    decorative={decorative}
+    orientation={orientation}
+    className={cn(
+      "shrink-0 bg-gray-200",
+      orientation === "horizontal"
+        ? "h-[1px] w-full"
+        : "h-full w-[1px]",
+      className
+    )}
+    {...props}
+  />
+))
+
+Separator.displayName = SeparatorPrimitive.Root.displayName
+
+export { Separator }
